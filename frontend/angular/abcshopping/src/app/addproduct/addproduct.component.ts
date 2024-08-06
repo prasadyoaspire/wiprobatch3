cb66f4a8884bbbea7d9ba0f0d14a2459918b48db
Prasad T <prasad.yoaspire@gmail.com>
2024-08-06 12:31:59 +0530

    mat style added to add product form

--- frontend/angular/abcshopping/src/app/addproduct/addproduct.component.ts
+++ frontend/angular/abcshopping/src/app/addproduct/addproduct.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../services/product.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-addproduct',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatFormFieldModule,MatInputModule,MatButtonModule],
   templateUrl: './addproduct.component.html',
   styleUrl: './addproduct.component.css'
 })
